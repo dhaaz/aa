c837efefcfd5def421750a98f8a07a34138e9434
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:17:47 +0700

    Add files via upload

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,1 +1,26 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+// components
+import Page from '../../components/page';
+import { CatalogBanner } from '../../components/banner';
+import { MovieGrid } from '../../modules/movies';
+// utils
+import { handleScrollToTop } from '../../utils';
 
+// ----------------------------------------------------------------------
+
+export default function Catalog() {
+  const { category } = useParams();
+  const isMovie = category === 'movies';
+
+  useEffect(() => {
+    handleScrollToTop();
+  }, [category]);
+
+  return (
+    <Page title={isMovie ? 'Film' : 'Series'}>
+      <CatalogBanner category={isMovie ? 'Film' : 'Series'} />
+      <MovieGrid category={isMovie ? 'movie' : 'tv'} />
+    </Page>
+  );
+};
