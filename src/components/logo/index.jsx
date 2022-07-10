3abc5ffae922b97c30d615e3c335fd58d78e9c24
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:08:21 +0700

    Add files via upload

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo-ax.png';
 
+// ----------------------------------------------------------------------
+
+export default function Logo({ height }) {
+  return (
+    <div className="">
+      <Link to="/">
+        <img src={logo} className={`${height} mt-3`} alt="" />
+      </Link>
+    </div>
+  );
+};
