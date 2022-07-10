853dce6ee856392a098bd9f77e869a83b03bd812
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:51:28 +0700

    Update embedApi.js

--- src/api/embedApi.js
+++ src/api/embedApi.js
@@ -4,6 +4,6 @@ import { EMBED_URL } from '../config';
 
 const embedMovie = (id) => `${EMBED_URL}=${id}`;
 const embedEpisode = (id, season, episode) =>
-  `${EMBED_URL}/${id}/${season}/${episode}`;
+  `${EMBED_URL}=${id}&sea={season}&epi=${episode}`;
 
 export { embedMovie, embedEpisode };
