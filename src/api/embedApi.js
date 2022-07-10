4537dc47459f95dc59c2ca19d2114459a4ea6a05
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:56:02 +0700

    Update embedApi.js

--- src/api/embedApi.js
+++ src/api/embedApi.js
@@ -3,7 +3,19 @@ import { EMBED_URL } from '../config';
 // ----------------------------------------------------------------------
 
 const embedMovie = (id) => `${EMBED_URL}=${id}`;
+
 const embedEpisode = (id, season, episode) =>
-  `${EMBED_URL}=${id}&{season}&${episode}`;
+
+`https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
 
 export { embedMovie, embedEpisode };
+
+
+
+
+
+
+
+
+
+
