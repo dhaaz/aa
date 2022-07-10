d4af37d3c35fa35839d4cf178eb007d83d6e2daf
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:12:26 +0700

    Add files via upload

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,1 +1,16 @@
-
+export default function Video({ src }) {
+  return (
+    <div
+      className="relative w-full duration-200 rounded-md overflow-hidden"
+      style={{ paddingBottom: '55%' }}
+    >
+      <iframe
+        title={src}
+        className="absolute top-0 left-0 w-full h-full z-10"
+        src={src}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
