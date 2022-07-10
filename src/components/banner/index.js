a95d6d65e868fabb9feac144db4aaf5795d50425
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 04:06:25 +0700

    Add files via upload

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,1 +1,3 @@
-
+export { default as HomeBanner } from './HomeBanner';
+export { default as CatalogBanner } from './CatalogBanner';
+export { default as BannerMovieDetails } from './BannerMovieDetails';
