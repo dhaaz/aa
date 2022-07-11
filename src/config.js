57996e9ca04afa18ace3e29d61496c5663e6a542
Dhaaz <75169859+dhaaz@users.noreply.github.com>
2022-07-11 07:02:23 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const TMDB_URL = process.env.REACT_APP_TMDB_URL || 'https://api.themoviedb.org/3/';
 export const TMDB_IMAGE_URL = process.env.REACT_APP_TMDB_IMAGE_URL || 'https://image.tmdb.org/t/p/';
 export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY || '79b890aa513c1b748265cdf52c88f840';
-export const EMBED_URL = process.env.REACT_APP_EMBED_URL || 'https://2embed.org/embed/tmdb';
+export const EMBED_URL = process.env.REACT_APP_EMBED_URL || 'https://2embed.org/embed/';
